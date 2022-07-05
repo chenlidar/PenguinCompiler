aece064f1d5e7a1a17d1d6146490a7e7ba251e81
chenlida <ChenLD3124@163.com>
2022-07-05 19:17:17 +0800

    temp

--- src/assem.h
+++ src/assem.h
@@ -16,26 +16,26 @@ struct Oper:public Instr{
     std::string assem;
     Temp_tempList dst,src;
     Targets jumps;
-    Oper(std::string _assem,Temp_tempList &_dst,Temp_tempList &_src,Targets _jumps):
+    Oper(std::string _assem,Temp_tempList _dst,Temp_tempList _src,Targets _jumps):
         assem(_assem),dst(_dst),src(_src),jumps(_jumps){}
     void print(){
         int i=0;
+        std::string out=assem;
         for(auto d:dst){
             std::string s=std::to_string(i++);
-            assem = assem.replace(assem.find("`d"+s), 1,std::to_string(d));
+            out = out.replace(out.find("`d"+s), 1,std::to_string(d));
         }
         i=0;
         for(auto sr:src){
             std::string s=std::to_string(i++);
-            assem = assem.replace(assem.find("`s"+s), 1,std::to_string(sr));
+            out = out.replace(out.find("`s"+s), 1,std::to_string(sr));
         }
-        std::cout<<assem<<std::endl;
+        std::cout<<out<<std::endl;
     }
 };
 struct Label:public Instr{
-    std::string assem;
     Temp_label label;
-    Label(std::string _assem,Temp_label _label):assem(_assem),label(_label){}
+    Label(Temp_label _label):label(_label){}
     void print(){
         std::cout<<label<<":"<<std::endl;
     }
@@ -46,9 +46,10 @@ struct Move:public Instr{
     Move(std::string _assem,Temp_temp _dst,Temp_temp _src):
         assem(_assem),dst(_dst),src(_src){}
     void print(){
-        assem = assem.replace(assem.find("`d0"), 1,std::to_string(dst));
-        assem = assem.replace(assem.find("`s0"), 1,std::to_string(src));
-        std::cout<<assem<<std::endl;
+        std::string out=assem;
+        out = out.replace(out.find("`d0"), 1,std::to_string(dst));
+        out = out.replace(out.find("`s0"), 1,std::to_string(src));
+        std::cout<<out<<std::endl;
     }
 };
 struct Proc{
