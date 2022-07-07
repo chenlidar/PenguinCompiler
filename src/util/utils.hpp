76d774eacedc74f6f381e9200fbf17f6110d1731
maze <782618517@qq.com>
2022-07-07 11:42:00 +0800

    ..

--- src/util/utils.hpp
+++ src/util/utils.hpp
@@ -1,6 +1,7 @@
 #ifndef __UTILS
 #define __UTILS
 #include "../structure/treeIR.hpp"
+#include "../structure/ty.hpp"
 inline IR::Stm* nopStm() {
     return (new IR::ExpStm(new IR::ConstInt(0)));
 }
@@ -17,5 +18,16 @@ static IR::Stm* seq(IR::Stm* x, IR::Stm* y) {
     if (isNop(y))
         return x;
     return new IR::Seq(x, y);
+}
+static TY::Type binopResType(const TY::Type& a, const TY::Type& b) {
+    if (a.kind != TY::tyType::Ty_float && a.kind != TY::tyType::Ty_int) {
+        // err
+    }
+    if (b.kind != TY::tyType::Ty_float && b.kind != TY::tyType::Ty_int) {
+        // err
+    }
+    return (b.kind == TY::tyType::Ty_float || a.kind != TY::tyType::Ty_float)
+               ? TY::floatType()
+               : TY::intType();
 }
 #endif
