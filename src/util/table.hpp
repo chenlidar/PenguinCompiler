9d04c67f7acf85db40b9bd4a4edcb11045d75486
maze <782618517@qq.com>
2022-07-12 20:38:03 +0800

    fix table

--- src/util/table.hpp
+++ src/util/table.hpp
@@ -2,10 +2,11 @@
 #define __TABLE
 
 #include <iostream>
+#include <iterator>
+#include <stack>
+#include <string>
 #include <unordered_map>
 #include <vector>
-#include <string>
-#include <stack>
 #include "assert.h"
 
 /*
@@ -15,124 +16,116 @@
 
 */
 
-namespace Table{
+namespace Table {
 
-    template <typename T_key,typename T_value>
-    class table
-    {
-    private:
-        std::unordered_map<T_key,T_value> map;
-        std::stack<std::pair<T_key,std::pair<T_value,bool> > > stk;
-    public:
-        table(/* args */){}
-        ~table(){}
+template <typename T_key, typename T_value>
+class table {
+   private:
+    std::unordered_map<T_key, T_value> map;
+    std::vector<std::pair<T_key, std::pair<T_value, bool>>> stk;
 
-        void enter(T_key,T_value);
-        bool exist(T_key);
-        T_value look(T_key);
-        std::pair<T_key,T_value> pop();
-    };
+   public:
+    table(/* args */) {}
+    ~table() {}
 
-    template <typename T_value>
-    class Stable
-    {
-    private:
-        table<std::string,T_value> table;
-    public:
-        Stable(/* args */){}
-        ~Stable(){}
-
-        void enter(std::string,T_value);
-        T_value look(std::string);
-        bool exist(std::string);
-        void beginScope(T_value useless);//anything  
-        void endScope();
-        std::pair<std::string,T_value> pop();
-    };
-
-    // implementation
-
-    template <typename T_key,typename T_value>
-    void table<T_key,T_value>::enter(T_key key,T_value value)
-    {
-        if(this->map.count(key))
-            this->stk.push(
-                std::make_pair(key,std::make_pair(map[key],true))
-            );
-        else
-            this->stk.push(
-                std::make_pair(key,std::make_pair(map[key],false))
-            );
-        map[key] = value;
+    void enter(T_key, T_value);
+    bool exist(T_key);
+    T_value look(T_key);
+    std::pair<T_key, T_value> pop();
+    std::vector<std::pair<T_key, std::pair<T_value, bool>>>::iterator begin() {
+        return stk.begin();
     }
-
-    template <typename T_key,typename T_value>
-    bool table<T_key,T_value>::exist(T_key key)
-    {
-        return map.find(key)!=map.end();
+    std::vector<std::pair<T_key, std::pair<T_value, bool>>>::iterator end() {
+        return stk.end();
     }
+};
 
-    template <typename T_key,typename T_value>
-    T_value table<T_key,T_value>::look(T_key key)
-    {
-        return map.find(key)->second;
-    }
+template <typename T_value>
+class Stable {
+   private:
+    table<std::string, T_value> table;
 
-    template <typename T_key,typename T_value>
-    std::pair<T_key,T_value> table<T_key,T_value>::pop()
-    {
-        assert(stk.size());
-        std::pair<T_key,T_value> ret = *map.find(stk.top().first);
-        if(stk.top().second.second)
-            map[stk.top().first] = stk.top().second.first;
-        else
-            map.erase(stk.top().first);
-        stk.pop();
-        
-        return ret;
-    }
+   public:
+    Stable(/* args */) {}
+    ~Stable() {}
 
+    void enter(std::string, T_value);
+    T_value look(std::string);
+    bool exist(std::string);
+    void beginScope(T_value useless);  // anything
+    void endScope();
+    std::pair<std::string, T_value> pop();
+};
 
-    //Stable
+// implementation
 
-    template <typename T_value>
-    void Stable<T_value>::enter(std::string str,T_value value)
-    {
-        this->table.enter(str,value);
-    }
-
-    template <typename T_value>
-    bool Stable<T_value>::exist(std::string str)
-    {
-        return this->table.exist(str);
-    }
-
-    template <typename T_value>
-    T_value Stable<T_value>::look(std::string str)
-    {
-        return this->table.look(str);
-    }
-
-    template <typename T_value>
-    void Stable<T_value>::beginScope(T_value useless)
-    {
-        this->enter(std::string("[_A__SCOPE_]"),useless);
-    }
-
-    template <typename T_value>
-    void Stable<T_value>::endScope()
-    {
-        while((this->pop().first) != std::string("[_A__SCOPE_]"));
-    }
-
-    template <typename T_value>
-    std::pair<std::string,T_value> Stable<T_value>::pop()
-    {
-        return this->table.pop();
-    }
-    
+template <typename T_key, typename T_value>
+void table<T_key, T_value>::enter(T_key key, T_value value) {
+    if (this->map.count(key))
+        this->stk.push_back(
+            std::make_pair(key, std::make_pair(map[key], true)));
+    else
+        this->stk.push_back(
+            std::make_pair(key, std::make_pair(map[key], false)));
+    map[key] = value;
 }
 
+template <typename T_key, typename T_value>
+bool table<T_key, T_value>::exist(T_key key) {
+    return map.find(key) != map.end();
+}
 
+template <typename T_key, typename T_value>
+T_value table<T_key, T_value>::look(T_key key) {
+    return map.find(key)->second;
+}
+
+template <typename T_key, typename T_value>
+std::pair<T_key, T_value> table<T_key, T_value>::pop() {
+    assert(stk.size());
+    std::pair<T_key, T_value> ret = *map.find(stk.back().first);
+    if (stk.back().second.second)
+        map[stk.back().first] = stk.back().second.first;
+    else
+        map.erase(stk.back().first);
+    stk.pop_back();
+
+    return ret;
+}
+
+// Stable
+
+template <typename T_value>
+void Stable<T_value>::enter(std::string str, T_value value) {
+    this->table.enter(str, value);
+}
+
+template <typename T_value>
+bool Stable<T_value>::exist(std::string str) {
+    return this->table.exist(str);
+}
+
+template <typename T_value>
+T_value Stable<T_value>::look(std::string str) {
+    return this->table.look(str);
+}
+
+template <typename T_value>
+void Stable<T_value>::beginScope(T_value useless) {
+    this->enter(std::string("[_A__SCOPE_]"), useless);
+}
+
+template <typename T_value>
+void Stable<T_value>::endScope() {
+    while ((this->pop().first) != std::string("[_A__SCOPE_]"))
+        ;
+}
+
+template <typename T_value>
+std::pair<std::string, T_value> Stable<T_value>::pop() {
+    return this->table.pop();
+}
+
+}  // namespace Table
 
 #endif
