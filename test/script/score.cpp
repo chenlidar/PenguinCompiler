a121028310c41691b814af7c5b4503667ea404c8
Tan-YiFan <15711657500@163.com>
2022-07-22 20:41:21 +0800

    Add score extract

---
+++ test/script/score.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+char m[200];
+char n[200];
+int main()
+{
+	string s;
+	while (cin >> s) {
+		int a, b, c, d;
+		scanf("%s %s\nTOTAL: %dH-%dM-%dS-%dus", m, n, &a, &b, &c, &d);
+		printf("%d.%06d\n", a*3600+b*60+c, d);
+	}
+}
