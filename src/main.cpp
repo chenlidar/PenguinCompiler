fb5020ed4e67039e45676de172d87d9962699b95
chenlida <ChenLD3124@163.com>
2022-08-04 12:37:26 +0800

    nothing

--- src/main.cpp
+++ src/main.cpp
@@ -169,7 +169,7 @@ int main(int argc, char** argv) {
             blocks = ssa->ssa2ir();
             out = CANON::traceSchedule(blocks);
             //
-            showir(out);
+            // showir(out);
             bool isvoid = fenv->look(funcname)->ty->tp->kind == TY::tyType::Ty_void;
             int stksize = fenv->look(funcname)->stksize;
             RA::RA_RegAlloc(CANON::funcEntryExit2(&IR::ir2asm(out)->body, isvoid, false), stksize);
